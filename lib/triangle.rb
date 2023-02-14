6e93946e518c0d8dbb8e800ef7bed260dfa98f11
antonyosebe <osebe.anto@gmail.com>
2023-02-14 11:02:36 +0300

    complete assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
   # write code here
+  attr_accessor :length1, :length2, :length3
+
+  def initialize(length1, length2, length3)
+    @length1 = length1
+    @length2 = length2
+  @length3 = length3
+  end
+
+  def kind
+    triangle = [@length1, @length2,@length3].uniq
+    p triangle
+    if @length1 <= 0 || @length2 <= 0 ||@length3 <= 0
+      raise TriangleError
+    end
+    if @length1 + @length2 <=@length3 || @length1 + @length3 <= @length2 || @length2 + @length3 <= @length1
+      raise TriangleError
+    end
+    if triangle.size == 1
+      :equilateral
+    elsif triangle.size == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
 end
